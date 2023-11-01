03339ff755581cc1354c9a888a645beaf88e4923
H. M. Masum Billah <mbillah21@gmail.com>
2023-11-01 06:02:35 +0600

    Assignment 03 Completed

--- task02.php
+++ task02.php
@@ -4,13 +4,13 @@ $number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function even($number)
 {
-
+    $oddNumber = [];
     foreach ($number as $num){
         if($num % 2 != 0){
-            unset($number[$num]);
+            $oddNumber[] = $num;
         }
     }
-    print_r($number);
+    print_r($oddNumber);
 }
 
 even($number);
